c7a15ae0683270dd2c802d6363408045504e6ce3
siddhu vydyabhushana <vydyas@gmail.com>
2024-10-12 11:53:34 +0530

    reverting pre-render middle ware

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    env: {
-      PRERENDER_TOKEN: process.env.PRERENDER_TOKEN,
-    },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'X-Prerender-Token',
-              value: process.env.PRERENDER_TOKEN || '',
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
